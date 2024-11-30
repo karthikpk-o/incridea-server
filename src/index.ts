312b4b842c1ced0c4612f97641c85f9a65a89c60
ahmedmsayeem <msayeemahmed76@gmail.com>
2024-11-30 21:48:53 +0530

    init[uploadthing]

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,12 @@ import { context } from "./context";
 import { schema } from "./schema";
 import bodyParser from "body-parser";
 import { handler as razorpayCapture } from "./webhook/capture";
-import { uploader as imageUpload } from "./cloudinary/upload";
-import { config } from "./cloudinary/config";
-import { config as easterConfig } from "./cloudinary/easterConfig";
-import { config as idUploadConfig } from "./cloudinary/idUpload";
-const { upload } = config;
-const { upload: easterUpload } = easterConfig;
-const { upload: idUpload } = idUploadConfig;
-// import "./certificate.ts";
+
 import { useDepthLimit } from "@envelop/depth-limit";
+import { createRouteHandler } from "uploadthing/express";
+import { uploadRouter } from "./uploadthing";
 const port = Number(process.env.API_PORT) || 4000;
+
 const yoga = createYoga({
   context,
   schema,
@@ -22,6 +18,11 @@ const yoga = createYoga({
 });
 
 const app = express();
+const authMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const authHeader = req.headers.authorization; 
+  console.log("Authorization Header:", authHeader);
+  next();
+};
 
 app.use(cors());
 
@@ -34,10 +35,36 @@ app.get("/", (_req, res) => {
 
 app.use("/graphql", yoga.requestListener);
 app.post("/webhook/capture", razorpayCapture);
-app.post("/cloudinary/upload/:eventName", upload.single("image"), imageUpload);
-app.post("/easter-egg/upload", easterUpload.single("image"), imageUpload);
-app.post("/id/upload", idUpload.single("image"), imageUpload);
+app.use("/uploadthing",authMiddleware,createRouteHandler({router: uploadRouter}),);
+
 
 app.listen(port, () => {
   console.log(`🚀 Server ready at: http://localhost:4000/graphql`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// const { upload } = config;
+// const { upload: easterUpload } = easterConfig;
+// const { upload: idUpload } = idUploadConfig;
+
+// app.post("/cloudinary/upload/:eventName", upload.single("image"), imageUpload);
+// app.post("/easter-egg/upload", easterUpload.single("image"), imageUpload);
+// app.post("/id/upload", idUpload.single("image"), imageUpload);
+
+// import { uploader as imageUpload } from "./cloudinary/upload";
+// import { config } from "./cloudinary/config";
+// import { config as easterConfig } from "./cloudinary/easterConfig";
+// import { config as idUploadConfig } from "./cloudinary/idUpload";
+
+// // import "./certificate.ts";
+// import jwt from 'jsonwebtoken';
---
+++ src/uploadthing.ts
@@ -0,0 +1,42 @@
+import { createUploadthing, type FileRouter } from "uploadthing/express";
+import { context } from "./context";
+const f = createUploadthing();
+
+export const uploadRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  eventUploader: f({
+    image: {
+      /**
+       * For full list of options and defaults and defaults, see the File Route API reference
+       * @see https://docs.uploadthing.com/file-routes#route-config
+       */
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  }).onUploadComplete((data) => {
+   console.log(context)
+    // create a new event
+    console.log("upload completed", data);
+  }),
+
+
+ easterEggUploader: f({
+    image:{
+        maxFileCount:1,
+        maxFileSize:"4MB"
+    },
+ }).onUploadComplete((data)=>{
+    console.log("easter Egg uploaded")
+ }),
+
+ idUploader: f({
+    image:{
+        maxFileCount:1,
+        maxFileSize:"4MB"
+    },
+ }).onUploadComplete((data)=>{
+    console.log("id uploaded")
+ })
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof uploadRouter;

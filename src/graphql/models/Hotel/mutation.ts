2c951831fe4cea21703eacdae00fd4ccfed36ddf
prabhuomkar9 <prabhuomkar9@gmail.com>
2025-02-20 01:42:36 +0530

    [fix] code logic

--- src/graphql/models/Hotel/mutation.ts
+++ src/graphql/models/Hotel/mutation.ts
@@ -16,8 +16,8 @@ builder.mutationField("createHotel", (t) =>
       const user = await ctx.user;
       if (!user) throw new Error("Not authenticated");
 
-      // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
       const isAllowed =
+        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
         checkIfAccommodationMember(user.id) || user.role === "ADMIN";
       if (!isAllowed) throw new Error("Not allowed to perform this action");
 
@@ -57,8 +57,8 @@ builder.mutationField("deleteHotel", (t) =>
       const user = await ctx.user;
       if (!user) throw new Error("Not authenticated");
 
-      // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
       const isAllowed =
+        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
         checkIfAccommodationMember(user.id) || user.role === "ADMIN";
       if (!isAllowed) throw new Error("Not allowed to perform this action");
 
